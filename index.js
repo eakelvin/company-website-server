fcdd3dc67525a30a07b9ebf6190ad7a95dc7e5b7
Kelvin Akaba <akabakelvin@gmail.com>
2024-02-21 14:14:38 +0000

    final commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express')
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 const server = express()
 const cors = require('cors')
 const connectDatabase = require('./src/config/database')
 const messageRoute = require('./src/routes/messageRoute')
 
 connectDatabase()
-server.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://localhost:3000",
-        "https://website-company-zeta.vercel.app"
-    ]
-}))
+// server.use(cors({
+//     origin: [
+//         "http://localhost:3000",
+//         "https://localhost:3000",
+//         "https://website-company-zeta.vercel.app"
+//     ]
+// }))
+server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
